885f95068510082e171d60dfd62a0f1207009665
RithikaC <88646906+RithikaC@users.noreply.github.com>
2021-08-09 11:58:14 +0530

    Add files via upload

---
+++ Roe/script.js
@@ -0,0 +1,28 @@
+//Krisha's section- Page 1 of the Wireframe
+
+
+
+
+
+
+
+
+//Rithika's section- Page 2 of the Wireframe
+
+
+
+
+
+
+
+
+// Natasha's section- Page 3 of the Wireframe
+
+
+
+
+
+
+
+
+//Athena's section- Page 4 of the Wireframe
